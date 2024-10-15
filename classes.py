e97e1dd7d80052cc97b67cf4d43e5584cf797612
likithar266 <154825695+likithar266@users.noreply.github.com>
2024-10-15 17:58:19 +0530

    Adding Classes

---
+++ classes.py
@@ -0,0 +1,36 @@
+# Organization class
+class Organization:
+    def __init__(self, name):
+        self.name = name
+        self.trainees = []  # List to keep track of trainees
+
+    def getName(self):
+        return self.name
+
+    def addTrainee(self, trainee):
+        self.trainees.append(trainee)
+
+    def getNumOfTrainees(self) -> int:
+        return len(self.trainees)
+
+
+# Trainer class
+class Trainer:
+    def __init__(self, name, organization):
+        self.name = name
+        self.organization = organization
+
+    def getOrganization(self):
+        return self.organization.getName()
+
+
+# Trainee class
+class Trainee:
+    def __init__(self, name, organization):
+        self.name = name
+        self.organization = organization
+        organization.addTrainee(self)  # adding the trainee to the organization
+
+    def getOrganization(self):
+        return self.organization.getName()
+
